6af55c67746bbd89befeb8d9f7f0bae813e50b76
Tatyana Druzhinina <dobro19936259@yandex.ru>
2024-08-13 22:48:31 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,7 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    boolean expected = true;
+	assertEquals("Возраст меньше 18 лет", expected, isAdult);
+}
